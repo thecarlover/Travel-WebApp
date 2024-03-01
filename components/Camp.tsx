b3613ef6fa0121a15e6f7199d888e04645ed0108
thecarlover <s1mittal@hotmail.com>
2024-03-01 12:43:51 +0530

    updated

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -17,7 +17,7 @@ const CampSite=({backgroundImage,title,subtitle,peoplejoined}:
     <div className={`h-full w-full min-w-[1100px] ${backgroundImage}
       bg-cover bg-center bg-no-repeat lg:rounded-r-5xl 2xl:rounded-5xl
     `}>
-      <div className="flex h-full flex-col items-start justify-between p-6 lg:px-20 lg:py-10 border-2 border-blue-500">
+      <div className="flex h-full flex-col items-start justify-between p-6 lg:px-20 lg:py-10 border-2 ">
         <div className="flexCenter gap-4">
           <div  className="rounded-full bg-green-50 p-4">
             <Image
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -14,7 +14,7 @@ const Hero = () => {
           height={50}
           className="absolute left-[-5px] top-[-30px] w-10 lg:w-[50px]"
         />
-        <h1 className="bold-52 lg:bold-88">Putuk Truno Camp Area</h1>
+        <h1 className="bold-52 lg:bold-88">Jim Corbett National </h1>
         <p className="regular-16 mt-6 text-gray-30 xl:max-w-[520px]">
           We want to be on each of your journeys seeking the satisfaction of seeing the incorruptible beauty of nature. We can help you on an adventure around the world in just one app
         </p>
@@ -61,7 +61,7 @@ const Hero = () => {
               <p className="regular-16 text-gray-20">Location</p>
               <Image src="/close.svg" alt="close" width={24} height={24} />
             </div>
-            <p className="bold-20 text-white">Aguas Calientes</p>
+            <p className="bold-20 text-white">Jim Corbett National</p>
           </div>
 
           <div className="flexBetween">
